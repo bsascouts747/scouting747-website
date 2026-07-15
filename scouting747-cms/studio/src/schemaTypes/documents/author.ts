2737a5f56e41c32f8c8624daf298545cd7d982ec
GroveKeith <keithrr26@gmail.com>
2026-07-14 20:26:19 -0500

    Fix: Solved syntax nesting error and updated author on post template

---
+++ scouting747-cms/studio/src/schemaTypes/documents/author.ts
@@ -0,0 +1,32 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'image',
+      title: 'Author Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
